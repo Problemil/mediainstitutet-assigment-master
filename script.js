55f9eea213a0564a444f8f3fc9296c2b26bcfc0d
Problemil <32762313+Problemil@users.noreply.github.com>
2017-10-26 16:15:57 +0200

    fixade klickfunktionen lite

--- script.js
+++ script.js
@@ -1,82 +1,81 @@
-
-
+//Javascritinlämning till David//
 /* GLOBAL VARIABLES */
-var listOfProducts;
-// add more global variables when needed..
 
-/* Get products from the json file and store it in a javascript variable */
-fetch("./products.json")
-.then(function(response) {
+var listOfProducts;
+//Json information//
+fetch("products.json")
+.then(function(response){
     return response.json();
-})
-.then(function(products) {
-    listOfProducts = products;
-    createUIFromLoadedProducts();
+} )
+.then(function(data) {
+    productData = data;
+    createListFromProductData();
+    console.log(productData);
 });
+//Gör loop och skapar variabel från "main" diven från index.html//
+    function createListFromProductData() {
+        var main = document.getElementById("main");
+        //loopar
+        for(var index = 0; index < productData.length; index++) {
+            //ringer den nya funktionen//
+            var productCard = createProductCard(productData[index]);
+            main.appendChild(productCard);
+        }
+        document.body.appendChild(main);
+    }
+//Helt ny funktion utanför den övre funktionen//
+    function createProductCard (productData) {
+        var productCard = document.createElement("div")
+        productCard.className = "productCardClass";
+//Title
+        var getProductTitle = document.createElement("h2");
+        getProductTitle.innerText = productData.title;
+        productCard.appendChild(getProductTitle);
+//Decepticon//
+        var getProductsDescription = document.createElement("p");
+        getProductsDescription.innerText = productData.description;
+        productCard.appendChild(getProductsDescription);
+//Price//
+        var getProductImage = document.createElement("img");
+        getProductImage.src = "assets/" + productData.image;
+        productCard.appendChild(getProductImage);
+//Button//
+        var getProductPrice = document.createElement("h3");
+        getProductPrice.innerText = productData.price + " kr";
+        productCard.appendChild(getProductPrice);
+
+        var buttonElement = document.createElement("button");
+        buttonElement.onclick = function() {positive()};
+        buttonElement.innerText = "Lägg till i kundvagn";
+        productCard.appendChild(buttonElement);
+
+   
+        return productCard;
+    }
+    var count = 0;
+    
+    var step = 1;
+    
+    
+    
+    var span = document.querySelector("span");
+    
+    var positive;
+    
+    
+    
+    function positive (){
+    
+       count = count + step
+    
+       span.innerText = count;
+    
+       alert("1 produkt har lagts till");
+    
+     }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function createUIFromLoadedProducts() {
     /* Check your console to see that the products are stored in the listOfProducts varible */
     console.log(listOfProducts);
-    var header = document.querySelector("header");
-    
-       header.innerHTML = '<a href="index.html" id="menubar">TechStore</a><span></span><a href="kundvagn.html" id="menubar2"><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>';
-
-    var main = document.querySelector("#main");
-    console.log(main);
-  
-
-
-/* iPhone nummer 1 */            
-    var iPhoneX = document.createElement("h1")
-    iPhoneX.innerText = (listOfProducts[0].title)
-    
-    main.appendChild(iPhoneX)
-
-    var iPhoneXdescription = document.createElement("p")
-    iPhoneXdescription.innerText = (listOfProducts[0].description)
-    
-    main.appendChild(iPhoneXdescription)
-
-    var img = document.createElement("img");
-    img.src = "assets/iPhoneX.png"
-
-    main.appendChild(img)
-
-    var iPhoneXPris = document.createElement("h2")
-    iPhoneXPris.innerText = (listOfProducts[0].price) + "kr";
-    
-    main.appendChild(iPhoneXPris)
-
-
-    /* Knapp 1 börjar */
-
-    var buttoniPhone = document.createElement("button");
-    buttoniPhone.innerHTML = `<i class="fa fa-shopping-cart fa-3x" aria-hidden="true"></i><h6>LÃ¤gg i kundvagnen<h6>`;
-    
-    var body = document.getElementsByTagName("#main")[0];
-    main.appendChild(buttoniPhone);
-    
-   
-
-    /* Knapp slut */
-/* iPhone slutar */ 
-
-console.log(listOfProducts[3])
-console.log(listOfProducts[0].price)
-
-/* Add your code here, remember to brake your code in to
-smaller function blocks to reduce complexity and increase readability */
-
-/* Each function must have an explainetory comment like the one for this function, see row 15 */
-
-/* Feel free to remove these other comments */
 }
-/*
-main.innerHTML = `<img src="assets/iPhoneX.png"/>
-<h1>iphoneX</h1>
-`
-*/
-
-/* Read the projects readme before you start! */
-/* Good luck and have fun ðŸ¤“ */
